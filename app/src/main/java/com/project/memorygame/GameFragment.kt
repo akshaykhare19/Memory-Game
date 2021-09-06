17ea40137d929dbdbffd0f7162bf9d5695f95ea1
Akshay Khare <akhare931@gmail.com>
2021-09-06 15:44:24 +0530

    Added Fragment

---
+++ app/src/main/java/com/project/memorygame/GameFragment.kt
@@ -0,0 +1,24 @@
+package com.project.memorygame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    companion object {
+
+        fun newInstance(): GameFragment{
+            return GameFragment()
+        }
+
+    }
+}
--- app/src/main/java/com/project/memorygame/MainActivity.kt
+++ app/src/main/java/com/project/memorygame/MainActivity.kt
@@ -2,10 +2,20 @@ package com.project.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.memorygame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.game_layout, GameFragment.newInstance())
+            .commit()
     }
 }
