e2a8e3382fcf96f4567d578c4ca98a0d9969f699
Akshay Khare <akhare931@gmail.com>
2021-09-06 16:34:46 +0530

    Added RecyclerView

---
+++ app/src/main/java/com/project/memorygame/GameView.kt
@@ -0,0 +1,30 @@
+package com.project.memorygame
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.project.memorygame.databinding.CardItemBinding
+
+class GameView(private val inputData: ArrayList<TextView>,
+                private val context: Context): RecyclerView.Adapter<GameViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        val adapterLayout = CardItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GameViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        val currentItem = inputData[position]
+        holder.binding.toString()
+    }
+
+    override fun getItemCount() = inputData.size
+
+
+}
+
+class GameViewHolder(val binding: CardItemBinding): RecyclerView.ViewHolder(binding.root){
+
+}
